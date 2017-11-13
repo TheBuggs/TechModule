f1e087dedca409105b947f8e521f49c433f13d26
Dobromir Popov <dbr.popov@abv.bg>
2017-11-14 01:16:09 +0200

    Second commit

--- E02A/Java/E02T01.java
+++ E02A/Java/E02T01.java
@@ -1,27 +1,41 @@
+//////////////////////////////////////////////////
+// Problem 2. 	Anonymous Threat
+//////////////////////////////////////////////////
+
 import java.util.Scanner;
 
-public class E02T01 {
+public class myJavaClass {
+	
     public static final String stop = "3:1";
 
-    public static String Marge(String str, int start, int end){
-        String[] arr = str.split(" ");
+    public static String Merge(String result, int start, int end){
+		
+        String[] arr = result.split(" ");
 
-        if(start < 0){
+        String str = "";
+
+        if(start < 0 || start > arr.length){
             start = 0;
         }
-        if(end > arr.length){
-            end = arr.length - 1;
+
+        if(end < 0 || end > arr.length){
+            end = arr.length;
         }
 
-        String temp = "";
+        if(start == end){
+            return result;
+        }
 
-        str = "";
         for(int i = 0; i < start; i++){
             str += arr[i] + " ";
         }
 
         for(int i = start; i < end; i++){
-            str += arr[i];
+            if(i == arr.length - 1){
+                str += arr[i] + " ";
+            }else {
+                str += arr[i];
+            }
         }
 
         for(int i = end; i < arr.length; i++){
@@ -31,12 +45,16 @@ public class E02T01 {
                 str += arr[i] + " ";
             }
         }
+
         return str;
     }
 
-    public static String Divide(String in, int item, int parts) {
-        String[] arr = in.split(" ");
+    public static String Divide(String result, int item, int parts) {
+		
+        String[] arr = result.split(" ");
+
         String str = "";
+
         if(item < 0) {
             item = 0;
         }
@@ -53,24 +71,24 @@ public class E02T01 {
             str += arr[i] + " ";
         }
 
-        int step = (arr[item].length() - 1) / parts;
+        int step = arr[item].length() / parts;
 
         char[] temp = arr[item].toCharArray();
 
         for(int i = 0, j = 0, c = 1; i < arr[item].length(); i++, j++) {
-            if(step * parts * c == j){
+			
+            if(step == j){
                 str += " ";
                 j = 0;
-                c++;
-            }else{
-                str += temp[i];
             }
-            if(i == arr[item].length() - 1){
+            str += temp[i];
+            if(i == arr[item].length() - 1 && arr.length - 1 == item){
                 str += " ";
             }
         }
 
         for(int i = item + 1; i < arr.length; i++){
+			
             if(i == arr.length - 1){
                 str += arr[i];
             }else {
@@ -84,16 +102,20 @@ public class E02T01 {
         Scanner scanner = new Scanner(System.in);
 
         String str = scanner.nextLine();
-
         String msg = "";
-        while(msg.equals("3:1") == false){
+
+        while(!msg.equals(stop)){
+
             msg = scanner.nextLine();
             String[] comm = msg.split(" ");
-            switch(comm[0]){
-                case "marge":  str = Marge(str, Integer.parseInt(comm[1]), Integer.parseInt(comm[2]));
-                break;
-                case "divide": str = Divide(str, Integer.parseInt(comm[1]), Integer.parseInt(comm[2]));
-                break;
+
+            switch(comm[0]) {
+                case "merge":
+                    str = Merge(str, Integer.parseInt(comm[1]), Integer.parseInt(comm[2]));
+                    break;
+                case "divide":
+                    str = Divide(str, Integer.parseInt(comm[1]), Integer.parseInt(comm[2]));
+                    break;
             }
         }
         System.out.println(str);
